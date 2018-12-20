624d0c35ff7f35779982286dbbe92c31fbf29a0a
marprok <mariosprokopakis@gmail.com>
2018-12-20 15:34:29 +0200

    checking of the functions for the same ids and name works

--- Visitor1.java
+++ Visitor1.java
@@ -45,70 +45,98 @@ public class Visitor1 extends DepthFirstAdapter
 		//	System.out.println(args.equals(other_args));
 		//	System.out.println(fName);
 		//	System.out.println(other.getId().toString());
-			if (other_args.size() == args.size()){
-				if(args.size() == 0){
+			if (other_args.size() == args.size())
+			{
+				if(args.size() == 0)
+				{
 					System.out.println("Line " + line + ": " +" Function " + fName +" is already defined");
 					return;
 				}
 				AArgument arg1 = (AArgument) args.get(0); 
 				AArgument arg2 = (AArgument) other_args.get(0);
 				
-				if(!(arg1.getId()).equals(arg2.getId())){ //elegxei an to onoma tou identifier tou enos einai idio me tou allou
+				// We have to check if all the ids are the same and if so, then we have to create an error for the user
+				if(!arg1.getId().getText().equals(arg2.getId().getText()))
+				{ //elegxei an to onoma tou identifier tou enos einai idio me tou allou
 					symtable.put(fName, node);
 					return;
 				}
+				System.out.println("test");
 				
-				boolean same_value = false;
+				//boolean same_value = false;
 				LinkedList list1 = arg1.getEqvalue();
 				LinkedList list2 = arg2.getEqvalue();
-				if (list1.size() == list2.size()){ // an einai idio to mege8os ton liston
-					if (list1.size() == 1){
+				/*
+				if (list1.size() == list2.size())
+				{ // an einai idio to mege8os ton liston
+					if (list1.size() == 1)
+					{
 						same_value = (((AEqvalue)list1.get(0)).getValue()).equals(((AEqvalue)list2.get(0)).getValue());
 					}
 					else
 						same_value = true;					
-				}
+				}*/
 					
 				list1 = arg1.getCommaid();	
 				list2 = arg2.getCommaid();
-				if(list1.size() == list2.size()){
-					boolean diff = false;
-					for(int i = 0; i < list1.size(); i++){
+				if(list1.size() == list2.size())
+				{
+					System.out.println(list1.size());
+					if (list1.size() == 0)
+					{
+						// there are no other arguments after the first one
+						System.out.println("Line " + line + ": " +" Function " + fName +" is already defined");
+						return;
+					}
+					//boolean diff = false;
+					for(int i = 0; i < list1.size(); i++)
+					{
 						ACommaid commaid1 = (ACommaid) list1.get(i);
 						ACommaid commaid2 = (ACommaid) list2.get(i);
-						if(!(commaid1.getId()).equals(commaid2.getId())){
+						
+						if(!(commaid1.getId().getText()).equals(commaid2.getId().getText()))
+						{
 							symtable.put(fName, node);
 							return;
 						}
-						else{
+						/*else
+						{
 							LinkedList newlist1 = commaid1.getEqvalue();
 							LinkedList newlist2 = commaid2.getEqvalue();
-							if(newlist1.size() == newlist2.size()){
-								if(newlist1.size() == 0){
+							if(newlist1.size() == newlist2.size())
+							{
+								if(newlist1.size() == 0)
+								{
 									if(!((AEqvalue)newlist1.get(0)).equals(((AEqvalue)newlist1.get(0))))
 									diff = true;
-							}
+								}
 								
-						}
-						if(!(list1.get(i)).equals(list2.get(i)))
-							diff = true;
+							}
+							if(!(list1.get(i)).equals(list2.get(i)))
+								diff = true;
 						}
 						if(diff)
 							symtable.put(fName, node);
 						else
 							System.out.println("Line " + line + ": " +" Function " + fName +" is already defined");
+						*/
 					}
+					// if we get here then all the ids after the first are the same
+					
+					System.out.println("Line " + line + ": " +" Function " + fName +" is already defined");
+					
 				}
-				else{
-					if(same_value)
+				else
+				{
+					/*if(same_value)
 						System.out.println("Line " + line + ": " +" Function " + fName +" is already defined");	
 					else
 						symtable.put(fName, node);
+					*/
 				}				
-			}else
-				symtable.put(fName, node);	
+			}
 		}
-		else
-			symtable.put(fName, node);
+		
+		symtable.put(fName, node);
     }
 }
---
+++ test4.py
@@ -0,0 +1,6 @@
+def func(x,y,z):
+	return 1
+def func(x,y):
+	return 2
+def func1(x,y,z):
+	return 3
